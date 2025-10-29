a3100c582487d8378375cfbe500b305b10bb2385
akpradhn <akpradhn@gmail.com>
2025-10-30 00:28:39 +0530

    Updates Backup and Deletons

---
+++ VaccineVault/VaccineVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VaccineVault
+//
+//  Created by Amit Kumar Pradhan on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
